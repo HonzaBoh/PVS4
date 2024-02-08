0be7c412e123e1ea336e219d47b1efb12fd59f4d
bohac <honza.boh00@gmail.com>
2024-02-08 16:05:54 +0100

    Threads - finalizace soubory

--- src/threads/fileReading/MultiReadingFinal.java
+++ src/threads/fileReading/MultiReadingFinal.java
@@ -7,49 +7,81 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class MultiReadingFinal extends Thread{
-    Queue<String> something = new LinkedList<>();
+    static Queue<String> filePaths = new LinkedList<>();
+    static LinkedList<MultiReadingFinal> workers = new LinkedList<>();
+//    Queue<MultiReadingFinal> synced = Collections.synchronizedCollection(workers);
 
-    static ArrayList<String> filePaths = new ArrayList<>();
+    final static int THREAD_COUNT = 8;
 
-    static Integer counter = 0;
+//    static ArrayList<String> filePaths = new ArrayList<>();
 
     @Override
     public void run() {
-
+        doStuff();
     }
 
-    void doStuff(String fileName){
+    void doStuff(){
         try {
+            String fileName = filePaths.poll();
             List<String> lines = Files.readAllLines(Paths.get(fileName));
             int[] array = new int[lines.size()];
             for (int i = 0; i < lines.size(); i++) {
                 array[i] = Integer.parseInt(lines.get(i));
             }
             Arrays.sort(array);
-
             System.out.println("Sorted array: " + this.getName() + " from " + fileName);
+//            workers.add(this);// jsem ready na dalsi ukol
         } catch (IOException e) {
             System.out.println(":(");
             throw new RuntimeException(e);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 50; i++) {
             filePaths.add("seq"+i+".txt");
         }
 
-        MultiReadingFinal[] mfs = new MultiReadingFinal[50];
-        //priprava vlaken
-        for (int i = 0; i < mfs.length; i++) {
-            mfs[i] = new MultiReadingFinal();
+//        MultiReadingFinal[] mfs = new MultiReadingFinal[50];
+//        //priprava vlaken
+//        for (int i = 0; i < mfs.length; i++) {
+//            mfs[i] = new MultiReadingFinal();
+//        }
+//
+//
+//        long start = System.currentTimeMillis();
+//        //jednotliva spousteni
+//        for (int i = 0; i < mfs.length; i++) {
+//            mfs[i].start();
+//        }
+//
+//        for (int i = 0; i < mfs.length; i++) {
+//            mfs[i].join();
+//        }
+//        long stop = System.currentTimeMillis();
+//        System.out.println("Sorting took " + (stop-start));
+
+        //singleThread
+//        long start = System.currentTimeMillis();
+//        MultiReadingFinal singleOne = new MultiReadingFinal();
+//        while (!filePaths.isEmpty()){
+//            singleOne.doStuff();
+//        }
+//        long stop = System.currentTimeMillis();
+//        System.out.println("Sorting took " + (stop-start));
+
+
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            workers.add(new MultiReadingFinal());
         }
 
-        //jednotliva spousteni
-        for (int i = 0; i < mfs.length; i++) {
-            mfs[i].start();
+        while (!filePaths.isEmpty()){
+            for (MultiReadingFinal mf : workers){
+                if (!mf.isAlive()){
+                    mf.start();
+                }
+            }
         }
-
 
     }
 }
